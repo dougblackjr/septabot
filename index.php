e97e357bc1d0224ed6196b140c7740d4a0803b8f
Doug Black <dblack@americanbible.org>
2017-11-15 12:50:50 -0500

    Initial template file

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+// Clear the variables we'll use
+
+// Set the train lines
+
+// Get input from user
+
+// Get train key from text sent in
+
+// Call SeptaStats API using cURL
+
+// Create reply
+
+// If we got a response (the array is not null)
+	// Let's get a count of trains
+// Else
+	// The user couldn't find their train, or they're being sassy
+
+
+// Send back json_encoded array
+
+// Flush the object
+
+// Enjoy!
