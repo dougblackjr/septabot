00eef35ec5a0f093afaf6ea8b5a9deb0ce9b3ac4
Doug Black <dblack@americanbible.org>
2017-11-30 11:34:50 -0500

    Updated with work so far

--- index.php
+++ index.php
@@ -1,10 +1,51 @@
 <?php
 
 // Clear the variables we'll use
+$trainName = '';
+$response = array(
+	'response_type' => 'ephemeral',
+	'text' => ''
+);
+$finalResponse = '';
 
 // Set the train lines
+$ch = curl_init();
+
+curl_setopt_array($ch, array(
+		CURLOPT_RETURNTRANSFER => 1,
+		CURLOPT_URL => 'http://www.septastats.com/api/current/lines'
+	)
+);
+
+$resp = curl_exec($ch);
+
+if(curl_errno($ch)) {
+	
+	die('CURL ERROR: ' . curl_errno($ch));
+
+}
+
+curl_close($ch);
+
+$decoded = json_decode($resp, TRUE);
+
+$allTrains = $decoded['data'];
 
 // Get input from user
+if (isset($_POST['command'])) {
+
+	$text = $_POST['text'];
+
+	if($text == 'help') {
+
+		$response['text'] = implode(' - ', $allTrains);
+
+	}
+
+
+}
+
+echo json_encode($response);
 
 // Get train key from text sent in
 
